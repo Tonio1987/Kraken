b8cf2a30f3b7e0ba62856bd24d477d258a3451fb
Anthony AVOLA <anthony.avola@gmail.com>
2019-12-08 15:00:52 +0100

    little code cleaning

--- Main.js
+++ Main.js
@@ -5,14 +5,25 @@ require('dotenv').config();
 const moment = require('moment');
 const cron = require('node-cron');
 
-// INIT SCHEDULERS
+
+// START
+console.log('************************************************************************');
+console.log('*****************            - FonZzTonio -            *****************');
+console.log('********************      --------------------      ********************');
+console.log('************************  KRAKEN TRADING ROBOT  ************************');
+console.log('********************      --------------------      ********************');
+console.log('*****************               2019 (c)               *****************');
+console.log('************************************************************************');
+console.log('-------> Starting server ...  ');
+
+console.log('-------> Scheduler initialization ...  ');
 const CTRL_CronScheduler = require('./controller/cron_controller/CTRL_CronScheduler');
 CTRL_CronScheduler.Init_CronScheduler();
 
-// INIT MAIN SCHEDULER
+console.log('-------> Main Scheduler initialization ...  ');
+cron.schedule('*/10 * * * * *', () => {
+    CTRL_CronScheduler.Reload_CronScheduler();
+});
 
-//cron.schedule('*/10 * * * * *', () => {
-//    CTRL_CronScheduler.Reload_CronScheduler();
-//});
-
+console.log('-------> Server started !');
 
