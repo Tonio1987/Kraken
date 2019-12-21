8a3e3403e36567036c8dfd4e6571982dd4136942
Anthony AVOLA <anthony.avola@gmail.com>
2019-12-21 22:01:42 +0100

    modify project struct - express + angulkar integr

--- myapp/app.js
+++ myapp/app.js
@@ -1,20 +1,25 @@
+// CALL TRADING ROBOT
+var robot = require('./private/TraderRobot');
+
+// CALL MODULES
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// CALL ROUTES
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
+// CALL API REST ROUTES
+
+
+// INIT APP
 var app = express();
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
-/* GET home page. */
+/* index of my site */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.resolve('views/test.html'));
 });
 
 module.exports = router;
